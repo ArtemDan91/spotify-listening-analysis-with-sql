45d7792a5641f83799a054d1ea5f78f209ae5b1d
ArtemDan91 <124268412+ArtemDan91@users.noreply.github.com>
2026-08-16 16:13:37 +0300

    Create 03_advanced _analysis.sql

---
+++ 03_advanced _analysis.sql
@@ -0,0 +1,7 @@
+-- ============================================================
+-- Spotify Listening Analysis
+-- Block 2: Advanced  SQL Analysis
+-- ============================================================
+
+
+-- 1. Calculate the percentage of all plays that were skipped.
