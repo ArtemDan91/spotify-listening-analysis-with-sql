60c8cacad354bffc6a33bda306ef6c384ea4ccad
ArtemDan91 <124268412+ArtemDan91@users.noreply.github.com>
2026-08-17 12:45:35 +0300

    Update 02_intermediate_analysis.sql

--- sql/02_intermediate_analysis.sql
+++ sql/02_intermediate_analysis.sql
@@ -96,7 +96,7 @@ SELECT
 FROM spotify_history
 GROUP BY track_name, artist_name
 HAVING COUNT(*) >= 50
-ORDER BY avg_listening_minutes DESC;
+ORDER BY avg_listening_minutes ASC;
 
 
 -- 8. Rank artists with at least 5 unique tracks by total listening time.
